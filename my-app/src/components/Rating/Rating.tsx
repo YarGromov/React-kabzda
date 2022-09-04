95144a061355e4aee69bfbd2df0b024716be23f8
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-04 16:57:39 +0200

    Title, Rating, Accordion

---
+++ my-app/src/components/Rating/Rating.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export function Rating(props: any) {
+    if(props.value === 0) {
+        return (
+            <div>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    )
+}
+
+function Star(props: any) {
+    if(props.selected) {
+        return <span><b>star</b></span>
+    } else {
+        return <span>star</span>
+    }
+
+}

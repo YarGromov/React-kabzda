94b566e1ba9c1b3119697df7e4d8d34004d10904
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-04 19:41:36 +0200

    useState OnOff

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
 
 function App() {
   return (
     <div className="App">
-        <PageTitle title={'This is APP component'}/>
-        <PageTitle title={'This is LOL'}/>
-        Article 1
-        <Rating value={1}/>
-        <Accordion titleValue={'Menu'} collapsed={true}/>
-        <Accordion titleValue={'Menu 2'} collapsed={false}/>
-        Article 2
-        <Rating value={0}/>
-        <Rating value={1}/>
-        <Rating value={2}/>
-        <Rating value={3}/>
-        <Rating value={4}/>
-        <Rating value={5}/>
+        {/*<PageTitle title={'This is APP component'}/>*/}
+        {/*<PageTitle title={'This is LOL'}/>*/}
+        {/*Article 1*/}
+        {/*<Rating value={1}/>*/}
+        {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+        {/*<Accordion titleValue={'Menu 2'} collapsed={false}/>*/}
+        {/*Article 2*/}
+        {/*<Rating value={0}/>*/}
+        {/*<Rating value={1}/>*/}
+        {/*<Rating value={2}/>*/}
+        {/*<Rating value={3}/>*/}
+        {/*<Rating value={4}/>*/}
+        {/*<Rating value={5}/>*/}
+        <OnOff/>
+
     </div>
   );
 }

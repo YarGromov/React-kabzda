cf4a3bb1cd4b26be4ed63f0ba7b7f9e018d29635
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-18 19:27:15 +0200

    Rating uncontoll value

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,37 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/Accordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(2)
+
   return (
     <div className="App">
-        {/*<PageTitle title={'This is APP component'}/>*/}
-        {/*<PageTitle title={'This is LOL'}/>*/}
-        {/*Article 1*/}
-        {/*<Rating value={1}/>*/}
-        {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+
+        <Rating value={ratingValue} onClick={setRatingValue}/>
+
+
+        {/*<OnOff/>*/}
         {/*<Accordion titleValue={'Menu 2'} collapsed={false}/>*/}
-        {/*Article 2*/}
-        {/*<Rating value={0}/>*/}
-        {/*<Rating value={1}/>*/}
-        {/*<Rating value={2}/>*/}
-        {/*<Rating value={3}/>*/}
-        {/*<Rating value={4}/>*/}
-        {/*<Rating value={5}/>*/}
-        <OnOff/>
 
-        <UncontrolledAccordion titleValue={"Menu"}/>
-        <UncontrolledRating/>
-
-
-        {/*<Accordion titleValue={'Menu 2'} collapsed={false}/>*/}
-        {/*<Rating value={3}/>*/}
-
-
+        {/*<UncontrolledAccordion titleValue={"Menu"}/>*/}
+        {/*<UncontrolledRating/>*/}
     </div>
   );
 }

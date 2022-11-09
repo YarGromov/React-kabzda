c154df19f595704c4407054bf5120f3e1149f28b
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-11-09 20:55:24 +0100

    not working useEffect

--- my-app/src/components/Counter/Counter.tsx
+++ my-app/src/components/Counter/Counter.tsx
@@ -1,17 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const Counter = () => {
 
     let [value, setValue] = useState(0)
 
+
+    useEffect(()=>{
+        let valueAsString = localStorage.getItem('CounterValue');
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }, [])
+
+    useEffect(()=>{
+        localStorage.setItem('CounterValue', JSON.stringify(value))
+    }, [value])
+
+
     const incHandler = () => {
         setValue(value + 1)
     }
 
-    const setToLocalStorageHandler = () => {
-        localStorage.setItem('CounterValue', JSON.stringify(value))
-        localStorage.setItem('CounterValue + 1', JSON.stringify(value + 1))
-    }
+    // const setToLocalStorageHandler = () => {
+    //     localStorage.setItem('CounterValue', JSON.stringify(value))
+    //     // localStorage.setItem('CounterValue + 1', JSON.stringify(value + 1))
+    // }
 
     const getFromLocalStorageHandler = () => {
         let valueAsString = localStorage.getItem('CounterValue');
@@ -21,23 +35,23 @@ export const Counter = () => {
         }
     }
 
-    const clearLocalStorageHandler = () => {
-        localStorage.clear()
-        setValue(0)
-    }
-
-    const removeItemFromLocalStorageHandler = () => {
-        localStorage.removeItem('CounterValue + 1')
-    }
+    // const clearLocalStorageHandler = () => {
+    //     localStorage.clear()
+    //     setValue(0)
+    // }
+    //
+    // const removeItemFromLocalStorageHandler = () => {
+    //     localStorage.removeItem('CounterValue + 1')
+    // }
 
     return (
         <div>
             <h1>{value}</h1>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-            <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-            <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+            {/*<button onClick={setToLocalStorageHandler}>setToLocalStorage</button>*/}
+            {/*<button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>*/}
+            {/*<button onClick={clearLocalStorageHandler}>clearLocalStorage</button>*/}
+            {/*<button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>*/}
         </div>
     );
 };

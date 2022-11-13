2728c817b46b53d7ff53a312b4068971ae462472
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-11-13 14:47:33 +0100

    Links

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,54 +17,66 @@ function App() {
 
     let [switchOn, setSwitchOn] = useState<boolean>(false)
 
-   const Profile = () => {
-       const params = useParams<'*'>()
-       const some = params["*"];
-       console.log(some)
-       return <div>frofile</div>
-   }
+    const Profile = () => {
+        const params = useParams<'*'>()
+        const some = params["*"];
+        console.log(some)
+        return <div>frofile</div>
+    }
 
-  return (
-    <div className="App">
-
-
-      <NavLink to={'/'}>main</NavLink>---
-      <NavLink to={'/login'}>login</NavLink>---
-      <NavLink to={'/frofile'}>frofile</NavLink>---
-      <NavLink to={'/frofile/1'}>frofile/1</NavLink>---
-
-        <Routes>
-            <Route path={'/*'} element={<div>404</div>}/>
-            <Route path={'/'} element={<div>main</div>}/>
-            <Route path={'/login'} element={<div>login</div>}/>
-            <Route path={'/frofile/*'} element={<Profile/>} />
-        </Routes>
-
-        {/*<UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}*/}
-
-        {/*<OnOff on={switchOn} onChange={setSwitchOn}/>*/}
-        {/*<Accordion titleValue={'Menu 2'}*/}
-        {/*           collapsed={accordionCollapsed}*/}
-        {/*           onChange={()=> {*/}
-        {/*               setAccordionCollapsed(!accordionCollapsed)*/}
-        {/*           }}*/}
-        {/*/>*/}
-
-        {/*<UncontrolledAccordion titleValue={"Menu"}/>*/}
-        {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
-        {/*<UncontrolledRating/>*/}
-        {/*<Counter/>*/}
+    return (
+        <div className="App">
 
 
-    </div>
-  );
+            <NavLink to={'/'}>main</NavLink>---
+            <NavLink to={'/login'}>login</NavLink>---
+            <NavLink
+                to={'/frofile'}
+                style={(params)=> {
+                    return  { color: params.isActive ? 'lime' : 'black'}
+                }}
+            >frofile</NavLink>---
+            <NavLink to={'/frofile/settings'}>settings</NavLink>---
+            <a href="https://www.udemy.com/course/javascript_full/learn/lecture/28490428#overview"
+               target={'_blank'}
+               rel={'noreferrer nofollow noopenner'}
+            >
+                ---xxx---
+            </a>
+
+            <Routes>
+                <Route path={'/*'} element={<div>404</div>}/>
+                <Route path={'/'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/frofile'} element={<div>frofile</div>}/>
+                <Route path={'/frofile/settings'} element={<div>settings</div>}/>
+            </Routes>
+
+            {/*<UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}*/}
+
+            {/*<OnOff on={switchOn} onChange={setSwitchOn}/>*/}
+            {/*<Accordion titleValue={'Menu 2'}*/}
+            {/*           collapsed={accordionCollapsed}*/}
+            {/*           onChange={()=> {*/}
+            {/*               setAccordionCollapsed(!accordionCollapsed)*/}
+            {/*           }}*/}
+            {/*/>*/}
+
+            {/*<UncontrolledAccordion titleValue={"Menu"}/>*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*<UncontrolledRating/>*/}
+            {/*<Counter/>*/}
+
+
+        </div>
+    );
 }
 
 type PageTitlePropsType = {
     title: string
 }
 
-function PageTitle(props: PageTitlePropsType){
+function PageTitle(props: PageTitlePropsType) {
     return <h1>
         {props.title}
     </h1>

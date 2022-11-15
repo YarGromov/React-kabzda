95916839a3df528bd6303a6f446ef514ccca686c
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-11-15 20:21:08 +0100

    useRef

--- my-app/src/stories/input.stories.tsx
+++ my-app/src/stories/input.stories.tsx
@@ -1,18 +1,31 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 
 export default {
     title: 'input'
 }
 
+
 export const UncontrolledInput = () => <input/>
 
 export const TrackValueOfUncontrolledInput = () => {
     const [value, setValue] = useState('');
-    return <><input onChange={(event)=>{
-    setValue(event.currentTarget.value)
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
     }
-    }/> - {value}</>
+    return <><input onChange={onChangeHandler}/> - {value}</>
 }
 
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
+        const elem = inputRef.current as HTMLInputElement;
+        setValue(elem.value)
+    }
+
+    return <><input ref={inputRef} id={'inputId'}/>
+        <button onClick={save}>save</button>
+        actual value: {value}</>
+}
 
 export const ControlledInput = () => <input value={'IT-Incubator'}/>

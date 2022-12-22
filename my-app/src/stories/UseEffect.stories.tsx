6404fe94fd6dac13ca7a2053f6efb37c9ce96cf0
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-12-22 16:44:39 +0100

    useEffect dependencies 2

--- my-app/src/stories/UseEffect.stories.tsx
+++ my-app/src/stories/UseEffect.stories.tsx
@@ -9,20 +9,26 @@ export const SimpleExample = () => {
     const [counter, setCounter] = useState(1);
     console.log('SimpleExample')
 
+    useEffect(()=>{
+        console.log('useEffect every render')
+        document.title = counter.toString();
+    })
+
+    useEffect(()=>{
+        console.log('useEffect only first render(componentDidMount)')
+        document.title = counter.toString();
+    }, [])
+
         useEffect(()=>{
-            console.log('useEffect')
+            console.log('useEffect first render and every counter change')
             document.title = counter.toString();
-            //api.getUsers().then('')
-            //setInterval
-            //indexedDB
-            //document.getElementId
-            //document.title = 'User'
         }, [counter])
 
     return (
         <div>
             Hello, {counter} {fake}
-            <button onClick={()=> setFake(fake + 1)}>+</button>
+            <button onClick={()=> setFake(fake + 1)}>fake+</button>
+            <button onClick={()=> setCounter(counter + 1)}>counter+</button>
         </div>
     );
 };

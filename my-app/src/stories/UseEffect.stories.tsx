1f61ff0268d0a09ed13c94c8e99528d094ed3a73
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-02-21 20:51:07 +0100

    KeysTackerExample

--- my-app/src/stories/UseEffect.stories.tsx
+++ my-app/src/stories/UseEffect.stories.tsx
@@ -62,3 +62,53 @@ export const SetIntervalExpample = () => {
         {/*<button  onClick={ () => setFake(fake + 1)} > Fake+ </button>*/}
     </>
 }
+
+export const ResetEffectExpample2 = () => {
+
+    const [counter, setCounter] = useState(1)
+    console.log('Component rendered with ' + counter)
+
+
+    useEffect(()=>{
+        console.log('Effect occurred: ' + counter)
+
+        return ()=>{
+            console.log('RESET EFFECT ' + counter)
+        }
+    },[counter])
+
+    const increase = () => {setCounter(counter + 1) }
+
+    return <>
+        Hello, Counter: {counter} <button onClick={increase}>+</button>
+
+    </>
+}
+
+export const KeysTackerExample = () => {
+
+    const [text, setText] = useState('')
+
+    console.log('Component rendered with ' + text)
+
+
+    useEffect(()=>{
+
+        const handler = (e:  KeyboardEvent)=>{
+            console.log(e.key);
+            setText((state)=> state + e.key)
+        }
+
+       window.addEventListener('keypress', handler)
+
+
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    },[])
+
+
+    return <>
+        Typed text: {text}
+    </>
+}
